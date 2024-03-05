34bce79d95614e7593c08c3635e7d7ac660c919f
Jill Leary <jileary@microsoft.com>
2024-03-05 17:50:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,69 @@
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Comment = require('./comment.model');
+
+var db = 'mongodb://localhost/comments';
+mongoose.connect(db);
+// Use body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Create a route
+app.get('/', function(req, res) {
+  res.send('happy to be here');
+});
+
+app.get('/comments', function(req, res) {
+  console.log('getting all comments');
+  Comment.find({})
+    .exec(function(err, comments) {
+      if (err) {
+        res.send('error occured')
+      } else {
+        console.log(comments);
+        res.json(comments);
+      }
+    });
+});
+
+app.get('/comments/:id', function(req, res) {
+  console.log('getting all comments');
+  Comment.findOne({
+      _id: req.params.id
+    })
+    .exec(function(err, comments) {
+      if (err) {
+        res.send('error occured')
+      } else {
+        console.log(comments);
+        res.json(comments);
+      }
+    });
+});
+
+app.post('/comment', function(req, res) {
+  var newComment = new Comment();
+
+  newComment.author = req.body.author;
+  newComment.text = req.body.text;
+
+  newComment.save(function(err, comment) {
+    if (err) {
+      res.send('error saving comment');
+    } else {
+      console.log(comment);
+      res.send(comment);
+    }
+  });
+});
+
+app.put('/comment/:id', function(req, res) {
+  Comment.findOneAndUpdate({
+    _id: req.params.id
+  }, {
+    $set: {
